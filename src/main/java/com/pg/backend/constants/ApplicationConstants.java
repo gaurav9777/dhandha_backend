4e632f3a52ac767297f1bdf184ab3303ecebe8a1
Atma Gaurav <atmagaurav@Atmas-MacBook-Pro.local>
2024-06-09 02:40:04 +0530

    testing dev branch , added constants for regular use

--- src/main/java/com/pg/backend/constants/ApplicationConstants.java
+++ src/main/java/com/pg/backend/constants/ApplicationConstants.java
@@ -5,4 +5,7 @@ import java.time.format.DateTimeFormatter;
 public class ApplicationConstants {
 
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    public static final String SUCCESS_MESSAGE = "success";
+    public static final Integer SUCCESS_STATUS_CODE = 200;
+
 }
